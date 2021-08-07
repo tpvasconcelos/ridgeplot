f026857c0a1e38718894c12a93bdedf9939084b4
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2021-08-08 01:38:29 +0200

    update scripts to run inside a __main__ block

--- examples/readme_simple.py
+++ examples/readme_simple.py
@@ -2,14 +2,20 @@ from numpy.random import normal
 
 from ridgeplot import ridgeplot
 
-# Put your real samples here...
-synthetic_samples = [normal(n / 1.2, size=600) for n in reversed(range(9))]
 
-# The `ridgeplot()` helper comes packed with sensible defaults
-fig = ridgeplot(samples=synthetic_samples)
+def main() -> None:
+    # Put your real samples here...
+    synthetic_samples = [normal(n / 1.2, size=600) for n in reversed(range(9))]
 
-# and the returned Plotly figure is still fully customizable
-fig.update_layout(height=500, width=800)
+    # The `ridgeplot()` helper comes packed with sensible defaults
+    fig = ridgeplot(samples=synthetic_samples)
 
-# show us the work!!
-fig.show()
+    # and the returned Plotly figure is still fully customizable
+    fig.update_layout(height=500, width=800)
+
+    # show us the work!!
+    fig.show()
+
+
+if __name__ == "__main__":
+    main()
