a2a83f4ee0fddd8d85c6a9bbeb7d2f89b885b86c
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2021-08-08 01:49:50 +0200

    refactor pytest_sessionstart() and document _patch_plotly_show()

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
-def pytest_sessionstart() -> None:
+from pytest import Session
+
+
+def _patch_plotly_show() -> None:
+    """Monkey patch ``plotly.io.show`` as to not perform any rendering and,
+    instead, simply call ``plotly.io._utils.validate_coerce_fig_to_dict``"""
     import plotly.io
 
     # noinspection PyProtectedMember
@@ -8,3 +13,16 @@ def pytest_sessionstart() -> None:
         validate_coerce_fig_to_dict(fig, validate)
 
     plotly.io.show = wrapped
+
+
+def pytest_sessionstart(session: Session) -> None:
+    """Called after the ``Session`` object has been created and before
+    performing collection and entering the run test loop.
+
+    :param pytest.Session session: The pytest session object.
+
+    References
+    ----------
+      * https://docs.pytest.org/en/6.2.x/reference.html#initialization-hooks
+    """
+    _patch_plotly_show()
