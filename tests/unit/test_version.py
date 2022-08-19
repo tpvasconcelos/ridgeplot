7f2882c953f89312c5a0f2228298ba3cee24e9cc
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2022-08-19 22:56:23 +0200

    Release version 0.1.18
    Signed-off-by: Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -3,4 +3,4 @@ def test_version() -> None:
     from ridgeplot._version import __version__ as v_private
 
     assert v_public is v_private
-    assert v_public == "0.1.17"
+    assert v_public == "0.1.18"
