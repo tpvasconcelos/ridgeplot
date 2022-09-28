ad901dc79f990997197007520019ba61432d4f16
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2022-09-28 13:44:00 +0200

    cleanup

--- scripts/upgrade_and_lock_requirements.py
+++ scripts/upgrade_and_lock_requirements.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import sys
 from pathlib import Path
+from typing import List
 
 ANSI_RED = "\033[31m"
 ANSI_RESET = "\033[0m"
@@ -33,26 +34,25 @@ def _exit(exit_code: int, print_err_message: bool = True) -> None:
     sys.exit(exit_code)
 
 
-def main() -> None:
-    _set_env_markers()
-
-    py_python_version = os.environ["PY_PYTHON_VERSION"]
-    py_sys_platform = os.environ["PY_SYS_PLATFORM"]
-    popen_args = [
-        "pip-compile-multi",
-        "--upgrade",
-        "--allow-unsafe",
-        "--in-ext",
-        "in",
-        "--out-ext",
-        f"{py_python_version}-{py_sys_platform}.txt",
-    ]
+def _run_subprocess(popen_args: List[str]) -> None:
     cmd_string = " ".join(popen_args)
     print(f"Running: {cmd_string}")
     try:
         subprocess.run(popen_args, check=True)
     except subprocess.CalledProcessError as exc:
         _exit(exc.returncode)
+
+
+def main() -> None:
+    _set_env_markers()
+    py_python_version = os.environ["PY_PYTHON_VERSION"]
+    py_sys_platform = os.environ["PY_SYS_PLATFORM"]
+
+    popen_args = [
+        'pip-compile-multi', '--upgrade', '--allow-unsafe',
+        '--out-ext', f'{py_python_version}-{py_sys_platform}.txt',
+    ]  # fmt: skip
+    _run_subprocess(popen_args)
 
     for reqs_txt in Path("requirements/").glob(f"*{py_python_version}-{py_sys_platform}.txt"):
         locked_path = Path("requirements/locked").joinpath(reqs_txt.name)
