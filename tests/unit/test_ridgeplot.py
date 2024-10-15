f8dffee58f51da1bc02d08baef6bde55ce9f3889
Tomas Pereira de Vasconcelos <tomasvasconcelos1@gmail.com>
2024-10-15 21:45:58 +0200

    Add `colorscale` unit tests

--- tests/unit/test_ridgeplot.py
+++ tests/unit/test_ridgeplot.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import pytest
+from _plotly_utils.exceptions import PlotlyError
 
 from ridgeplot import ridgeplot
 from ridgeplot._colors import get_colorscale
@@ -55,11 +56,41 @@ def test_y_labels_dedup() -> None:
     )  # fmt: skip
 
 
-def test_colorscale_equivalence() -> None:
+@pytest.mark.parametrize("name", ["default", "bluered", "viridis", "plasma"])
+def test_colorscale_by_name(name: str) -> None:
     assert (
-        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale="bluered") ==
-        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale=get_colorscale("bluered"))
+        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale=name) ==
+        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale=get_colorscale(name))
     )  # fmt: skip
+
+
+def test_colorscale_invalid_name() -> None:
+    with pytest.raises(ValueError, match="Unknown colorscale name"):
+        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale="whodis")
+
+
+def test_colorscale_invalid_values() -> None:
+    colorscale = (
+        (0.0, "rgb(0, 0, 0)"),
+        (1.2, "rgb(0, 0, 0)"),
+    )
+    with pytest.raises(
+        PlotlyError,
+        match=r"The first and last number in your scale must be 0\.0 and 1\.0 respectively",
+    ):
+        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale=colorscale)
+
+
+def test_colorscale_invalid_colors() -> None:
+    colorscale = (
+        (0.0, "rgb(0, 0, 0)"),
+        (1.0, "not a valid color"),
+    )
+    with pytest.raises(
+        ValueError,
+        match="color should be a tuple or a str representation of a hex or rgb color",
+    ):
+        ridgeplot(samples=[[[1, 2, 3], [4, 5, 6]]], colorscale=colorscale)
 
 
 def test_deprecated_colormode_index() -> None:
