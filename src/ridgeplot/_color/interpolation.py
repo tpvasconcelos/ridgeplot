04408583d06c9a618bea909cc30b72dfe7b41081
Stephany <santosstephany400@gmail.com>
2024-11-04 19:52:10 -0300

    Refactoring return

--- src/ridgeplot/_color/interpolation.py
+++ src/ridgeplot/_color/interpolation.py
@@ -116,7 +116,7 @@ def _mul(a: tuple[Numeric, ...], b: tuple[Numeric, ...]) -> tuple[Numeric, ...]:
 
 def _interpolate_row_index(ctx: InterpolationContext) -> ColorscaleInterpolants:
     if ctx.n_rows == 1:
-        return [[0.0] * len(row) for row in ctx.densities]
+        return [[0.0]]
     return [
         [((ctx.n_rows - 1) - ith_row) / (ctx.n_rows - 1)] * len(row)
         for ith_row, row in enumerate(ctx.densities)
@@ -125,7 +125,7 @@ def _interpolate_row_index(ctx: InterpolationContext) -> ColorscaleInterpolants:
 
 def _interpolate_trace_index(ctx: InterpolationContext) -> ColorscaleInterpolants:
     if ctx.n_traces == 1:
-        return [[0.0] * len(row) for row in ctx.densities]
+        return [[0.0]]
     ps = []
     ith_trace = 0
     for row in ctx.densities:
@@ -139,7 +139,7 @@ def _interpolate_trace_index(ctx: InterpolationContext) -> ColorscaleInterpolant
 
 def _interpolate_trace_index_row_wise(ctx: InterpolationContext) -> ColorscaleInterpolants:
     if ctx.n_rows == 1:
-        return [[0.0] * len(row) for row in ctx.densities]
+        return [[0.0]]
     return [
         [((len(row) - 1) - ith_row_trace) / (len(row) - 1) for ith_row_trace in range(len(row))]
         for row in ctx.densities
